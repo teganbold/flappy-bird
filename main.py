f165b62ece1c8b2850b14d3e567f5ceba9c57c72
Tegan Bold <tegan.bold@gmail.com>
2024-06-03 18:14:59 -0400

    Add moving pipes

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ screen_width = 375
 #Game Variables
 ground_scroll = 0
 scroll_speed = 4
+pipe_gap = 150
+pipe_refresh = 1500 #miliseconds
+last_pipe = pygame.time.get_ticks()
 flying = False
 game_over = False
 
@@ -94,11 +97,36 @@ class Bird(pygame.sprite.Sprite):
             #Animate Rotation.
             self.image = pygame.transform.rotate(self.images[self.index], self.vel * -2.5)
 
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y, position) -> None:
+        global pipe_gap
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("sprites/pipe-green.png")
+        self.rect = self.image.get_rect()
+        # Position 1:  Pipe coming from the top
+        # Position -1: Pipe coming from the bottom
+        if position == 1:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.bottomleft = [x, y - (pipe_gap // 2)]
+        if position == -1:
+            self.rect.topleft = [x, y + (pipe_gap // 2)]
+
+    def update(self):
+        global scroll_speed
+        self.rect.x -= scroll_speed
+        
 
 bird_group = pygame.sprite.Group()
+pipe_group = pygame.sprite.Group()
 
 flappy = Bird(50, screen_height // 2)
+top_pipe = Pipe(250, screen_height // 2, 1)
+btm_pipe = Pipe(250, screen_height // 2, -1)
+
 bird_group.add_internal(flappy)
+pipe_group.add_internal(top_pipe)
+pipe_group.add_internal(btm_pipe)
+
 
 
 run = True
@@ -111,14 +139,16 @@ while run == True:
 
     bird_group.draw(screen)
     bird_group.update()
+    pipe_group.draw(screen)
+    pipe_group.update()
 
     #Draw and scroll the ground
     screen.blit(ground_image, (ground_scroll, 680))
     if not game_over:
+        #Scroll the ground
         ground_scroll -= scroll_speed
         if abs(ground_scroll) > 25:
             ground_scroll = 0
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -130,4 +160,3 @@ while run == True:
     pygame.display.update()
 
 # if __file__ == '__main__':
-#     flappybird.play()
